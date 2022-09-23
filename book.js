47c7638c46eeefc4e21a481df3602961a83c0b3c
athulprakash007 <athull362@gmail.com>
2022-09-23 11:53:47 -0700

    completed

---
+++ book.js
@@ -0,0 +1,39 @@
+bookbtnevent=()=>
+{
+    name=(document.getElementById("name").value)
+    phoneno=(document.getElementById("phoneno").value)   
+    emailid=(document.getElementById("emailid").value)   
+    moviename=(document.getElementById("moviename").value)
+    numberoftickets=(document.getElementById("tickets").value)
+    date=(document.getElementById("date").value)
+    time=(document.getElementById("time").value)
+   city=(document.getElementById("city").value)
+
+       
+
+    console.log(name)
+    alert(name)
+
+    console.log(phoneno)
+    alert(phoneno)
+
+    console.log(emailid)
+    alert(emailid)
+
+    console.log(moviename)
+    alert(moviename)
+
+    console.log(numberoftickets)
+    alert(numberoftickets)
+
+    console.log(date)
+    alert(date)
+    
+
+    console.log(time)
+    alert(time)
+
+    console.log(city)
+    alert(city)
+
+}
